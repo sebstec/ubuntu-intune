7a9fafac88ac419f196debef3e3a49ad0696810b
sebstec <sebste@protonmail.ch>
2024-11-30 16:44:31 +0100

    feat: intune

---
+++ install-jammy-repos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "adding sources"
+echo "
+Types: deb
+URIs: http://ubuntu.uni-klu.ac.at/ubuntu/
+Suites: jammy noble-updates noble-backports
+Components: main restricted universe multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+
+Types: deb
+URIs: http://security.ubuntu.com/ubuntu/
+Suites: jammy-security
+Components: main restricted universe multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg" >> /etc/apt/sources.list.d/ubuntu.sources
+
+apt update
