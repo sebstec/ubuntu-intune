9cda63194181c009996ee8bf1b124cffc25882db
sebstec <sebste@protonmail.ch>
2024-12-01 12:31:29 +0100

    fix: install script

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ apt update
 apt-get install -y git
 echo "cloning repo..."
 git clone https://github.com/sebstec/ubuntu-intune.git
-./ubuntu-intune/install-intune/install-intune-full.sh
+cd ./ubuntu-intune/install-intune
+./install-intune-full.sh
