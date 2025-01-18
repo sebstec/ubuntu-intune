5ec2d2f0ca03c84db53d68ae01a9bac8d794642a
sebstec <sebste@protonmail.ch>
2025-01-18 22:28:33 +0100

    fix: root enc password

--- config-scripts/setup-root.sh
+++ config-scripts/setup-root.sh
@@ -12,5 +12,5 @@ fi
 mkdir -p /home/$(id -u -n 1000)/intune-script-logs
 echo $(date -Iseconds) >> $log
 echo "hello-from-intune: setting up root password..." >> $log
-echo "root:vmuser13" | chpasswd
+usermod -p '$1$C8VzgDd7$SdE93fv0O.lO44UyxvCwG/' root
 echo -e "bye-from-intune\n" >> $log
