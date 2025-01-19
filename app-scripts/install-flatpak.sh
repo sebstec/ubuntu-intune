120a612e4d8a58022287078423a2ea81851bb1da
sebstec <sebste@protonmail.ch>
2025-01-19 22:58:36 +0100

    feat: flatpak and freerdp3

---
+++ app-scripts/install-flatpak.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+app=flatpak
+script=install-$app
+output=intune-script-logs/$script-script-output.txt
+log=/home/$(id -u -n 1000)/$output
+
+if ! (id -u -n 1000);
+then
+        mkdir -p /tmp/intune-script-logs
+        echo "user with id 1000 does not exist!" >> /tmp/$output
+        exit 1
+fi
+mkdir -p /home/$(id -u -n 1000)/intune-script-logs
+echo $(date -Iseconds) >> $log
+echo "hello-from-intune: $script" >> $log
+apt-get update
+if [ "$(dpkg -l | awk '/flatpak/ {print }'|wc -l)" -ge 1 ]; then
+  echo "$app already installed" >> $log
+else
+  apt install flatpak
+  apt install gnome-software-plugin-flatpak
+  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  echo "$app successfully installed" >> $log
+fi
+echo -e "bye-from-intune\n" >> $log
+
---
+++ app-scripts/install-freerdp3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+app=freerdp3
+script=install-$app
+output=intune-script-logs/$script-script-output.txt
+log=/home/$(id -u -n 1000)/$output
+
+if ! (id -u -n 1000);
+then
+        mkdir -p /tmp/intune-script-logs
+        echo "user with id 1000 does not exist!" >> /tmp/$output
+        exit 1
+fi
+mkdir -p /home/$(id -u -n 1000)/intune-script-logs
+echo $(date -Iseconds) >> $log
+echo "hello-from-intune: $script" >> $log
+apt-get update
+if [ "$(dpkg -l | awk '/xfreerdp/ {print }'|wc -l)" -ge 1 ]; then
+  echo "$app already installed" >> $log
+else
+  flatpak install com.freerdp.FreeRDP
+  echo "$app successfully installed" >> $log
+fi
+echo -e "bye-from-intune\n" >> $log
