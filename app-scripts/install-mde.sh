8ac164ec679d3252d8588d882a1459ed9356629d
sebstec <sebste@protonmail.ch>
2025-01-20 20:44:05 +0100

    fix: mde scripts

--- app-scripts/install-mde.sh
+++ app-scripts/install-mde.sh
@@ -2,6 +2,7 @@
 wget https://raw.githubusercontent.com/microsoft/mdatp-xplat/refs/heads/master/linux/installation/mde_installer.sh
 chmod 755 ./mde_installer.sh
 read -p 'put the onboarding python script downloaded from ms defender portal to this directory! then press a key...'
-sudo ./mde_installer.sh --install --channel prod --onboard ./MicrosoftDefenderATPOnboardingLinuxServer.py --tag GROUP Coders --min_req -y
+./mde_installer.sh --install --channel prod --onboard ./MicrosoftDefenderATPOnboardingLinuxServer.py --tag GROUP Coders --min_req -y
+mdatp config real-time-protection --value enabled
 mdatp health --field real_time_protection_enabled
 curl -o ~/Downloads/eicar.com.txt https://www.eicar.org/download/eicar.com.txt
--- app-scripts/test-mde.sh
+++ app-scripts/test-mde.sh
@@ -2,5 +2,8 @@
 echo "testing mde real time protection, first checking if it is enabled:"
 mdatp health --field real_time_protection_enabled
 read -p 'if the output says its disabled, you should enable this and then rerun this test. press a key to continue...'
-curl -o ~/Downloads/eicar.com.txt https://www.eicar.org/download/eicar.com.txt
+curl -o /tmp/eicar_com.zip https://secure.eicar.org/eicar_com.zip
+curl -o /tmp/eicarcom2.zip https://secure.eicar.org/eicarcom2.zip
 echo "downloaded a test file, check md portal in a few minutes for an alert"
+echo "threats locally detected:"
+mdatp threat list
