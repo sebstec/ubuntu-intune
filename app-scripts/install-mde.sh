a69eb54a31a94f456465cb1e36eb122020399a9e
sebstec <sebste@protonmail.ch>
2025-01-20 21:00:58 +0100

    fix: mde script

--- app-scripts/install-mde.sh
+++ app-scripts/install-mde.sh
@@ -19,7 +19,7 @@ if [ "$(dpkg -l | awk '/mdatp/ {print }'|wc -l)" -ge 1 ]; then
 else
   wget https://raw.githubusercontent.com/microsoft/mdatp-xplat/refs/heads/master/linux/installation/mde_installer.sh
   chmod 755 ./mde_installer.sh
-  if [ "$(ls | grep -ic 'MicrosoftDefenderATPOnboardingLinuxServer.py')" -e 0 ]; then
+  if [ "$(ls | grep -ic 'MicrosoftDefenderATPOnboardingLinuxServer.py')" -le 0 ]; then
     echo "put the onboarding python script downloaded from ms defender portal to this directory: $(pwd)!"
   else
   ./mde_installer.sh --install --channel prod --onboard ./MicrosoftDefenderATPOnboardingLinuxServer.py --tag GROUP Coders --min_req -y
