c708e37837fe6303ad8e0559ebf0608c368caf67
sebstec <sebste@protonmail.ch>
2025-01-20 20:29:11 +0100

    fix: dl url mde

--- app-scripts/install-mde.sh
+++ app-scripts/install-mde.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://github.com/microsoft/mdatp-xplat/blob/master/linux/installation/mde_installer.sh
+wget https://raw.githubusercontent.com/microsoft/mdatp-xplat/refs/heads/master/linux/installation/mde_installer.sh
 chmod 755 ./mde_installer.sh
 read -p 'put the onboarding python script downloaded from ms defender portal to this directory! then press a key...'
 sudo ./mde_installer.sh --install --channel prod --onboard ./MicrosoftDefenderATPOnboardingLinuxServer.py --tag GROUP Coders --min_req -y
