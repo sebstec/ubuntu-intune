6392eedf44f6222665e8cb79fc54812806d41f1d
sebstec <sebste@protonmail.ch>
2025-02-11 19:13:56 +0100

    fix: configure v4l2loopback

--- config-scripts/configure-v4l2loopback-at-boot.sh
+++ config-scripts/configure-v4l2loopback-at-boot.sh
@@ -13,6 +13,8 @@ mkdir -p /home/$(id -u -n 1000)/intune-script-logs
 echo $(date -Iseconds) >> $log
 echo "hello-from-intune: configuring v4l2loopback at boot..." >> $log
 echo "v4l2loopback" > /etc/modules-load.d/v4l2loopback.conf
-echo "options v4l2loopback video_nr=9 card_label=Video-Loopback exclusive_caps=1" > /etc/modprobe.d/v4l2loopback.conf
+# also tested working
+#echo "options v4l2loopback video_nr=9 card_label=Video-Loopback exclusive_caps=1" > /etc/modprobe.d/v4l2loopback.conf
+echo "options v4l2loopback devices=1 video_nr=10 card_label=\"OBS Cam\" exclusive_caps=1" > /etc/modprobe.d/v4l2loopback.conf
 # update-initramfs -c -k $(uname -r)
 echo -e "bye-from-intune\n" >> $log
