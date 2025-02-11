88d8e5f431989f96ba11f2ac90b3d1b459cc59e0
sebstec <sebste@protonmail.ch>
2025-02-11 18:55:17 +0100

    fix: obs

--- app-scripts/install-obs.sh
+++ app-scripts/install-obs.sh
@@ -19,13 +19,25 @@ if [ "$(dpkg -l | awk '/obs/ {print }'|wc -l)" -ge 1 ]; then
 fi
 #else
   # obs
-  add-apt-repository -y ppa:obsproject/obs-studio
+  echo "installing curl..." >> $log
+  apt install -y curl
+  echo "curling obs source key..." >> $log
+  curl https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xbc7345f522079769f5bbe987efc71127f425e228 | gpg --dearmor > obs.gpg
+  echo "installing obs source key..." >> $log
+  install -o root -g root -m 644 obs.gpg /etc/apt/trusted.gpg.d/
+  echo "installing source..." >> $log
+  echo "deb https://ppa.launchpadcontent.net/obsproject/obs-studio/ubuntu noble main
+deb-src https://ppa.launchpadcontent.net/obsproject/obs-studio/ubuntu noble main" > /etc/apt/sources.list.d/obs.list
+  echo "apt updating and installing ffmpeg and obs-studio..." >> $log
   apt update
   apt install -y ffmpeg obs-studio
   # background removal plugin
-  wget "https://github.com/locaal-ai/obs-backgroundremoval/releases/download/1.1.13/obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb" -O ./obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb
-  dpkg -i obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb
-  # rm obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb
+  echo "getting obs backgroundremoval plugin installer..." >> $log
+  curl https://github.com/locaal-ai/obs-backgroundremoval/releases/download/1.1.13/obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb -LJ -o /tmp/obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb
+  echo "installing obs backgroundremoval plugin..." >> $log
+  yes | dpkg -i /tmp/obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb
+  echo "removing obs backgroundremoval plugin installer..." >> $log
+  rm /tmp/obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb
   # loopback to get virtual cam
   # should be installed with intune-portal, as it requires manual interaction if secure-boot is enabled
   # apt install -y linux-headers-$(uname -r) v4l2loopback-dkms
