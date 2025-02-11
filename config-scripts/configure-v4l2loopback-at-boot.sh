717a5239c96bfd8c1849923e981d888e1f8c887a
sebstec <sebste@protonmail.ch>
2025-02-11 19:08:42 +0100

    feat: add config to run v4l2loopback at boot

---
+++ config-scripts/configure-v4l2loopback-at-boot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+script=configure-v4l2loopback-at-boot
+output=intune-script-logs/$script-script-output.txt
+log=/home/$(id -u -n 1000)/$output
+
+if ! (id -u -n 1000); 
+then
+	mkdir -p /tmp/intune-script-logs
+	echo "user with id 1000 does not exist!" >> /tmp/$output
+	exit 1
+fi
+mkdir -p /home/$(id -u -n 1000)/intune-script-logs
+echo $(date -Iseconds) >> $log
+echo "hello-from-intune: configuring v4l2loopback at boot..." >> $log
+echo "v4l2loopback" > /etc/modules-load.d/v4l2loopback.conf
+echo "options v4l2loopback video_nr=9 card_label=Video-Loopback exclusive_caps=1" > /etc/modprobe.d/v4l2loopback.conf
+# update-initramfs -c -k $(uname -r)
+echo -e "bye-from-intune\n" >> $log
